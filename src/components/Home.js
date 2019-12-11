6eff4cbec4a654fb53fd45774551583739fa626e
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-11 19:28:17 +0530

    add form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
 
-export default class Home extends Component {
-  handleSubmit(event) {
-    event.preventDefault();
-    let name = this.name.value;
-    let topic = this.topic.value;
-    let path = `people/${name}/${topic}`;
+class Home extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    let teacherTopic = this.topic.value;
+    let teacherName = this.name.value;
+    let path = `people/${teacherTopic}/${teacherName}`;
     this.props.history.push(path);
-  }
+  };
   render() {
     return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      <div>
         <form onSubmit={this.handleSubmit}>
-          <label>name</label>
-          <input ref={input => (this.name = input)} />
-          <label>topic</label>
-          <input ref={input => (this.topic = input)} />
-          <button type={"submit"}>Submit</button>
+          <input
+            type="text"
+            placeholder="Name"
+            ref={input => (this.name = input)}
+          />
+          <input
+            type="text"
+            placeholder="Topic"
+            ref={input => (this.topic = input)}
+          />
+          <button type="submit">Go!</button>
         </form>
       </div>
     );
   }
 }
+
+export default Home;
